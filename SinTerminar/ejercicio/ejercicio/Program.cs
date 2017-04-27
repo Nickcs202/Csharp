55f30049346f5c33b737bc28da57c5a5354edde1
Nickcs202 <nikc202@hotmail.com>
2017-04-27 11:09:13 -0300

    Codigo prueba matriz

---
+++ SinTerminar/ejercicio/ejercicio/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ejercicio
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			//Console.WriteLine ("Hello World!");
+			int[,] d = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+			int[] a=new int[12];
+			a=det (d);
+			for (int i = 0; i < 12; i++) {
+				Console.WriteLine (a[i]+" ");
+			}
+		}
+
+		public static int[] det (int[,] d)
+		{
+			int n = 0;
+			int[] aux=new int[12];
+
+			for (int k = 0; k < 3; k++) {
+				for (int i = 0; i < 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						//Console.WriteLine ("");
+						if (i != 0) {
+							if (j != k) {
+								Console.WriteLine ("D= " + d [i, j]);
+								aux [n] = d [i, j];
+								n++;
+							}
+						}
+					}
+				}
+				Console.WriteLine ("");
+			}
+			return aux;
+		}
+
+	}
+}
